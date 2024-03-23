5f14e5686611771ed364142e119987e19db41161
Marcus Bosveld <mjh.bosveld@student.han.nl>
2024-03-23 17:51:39 +0100

    Door de boom heen lopen gefixt

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -4,6 +4,7 @@ import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.AST;
 import nl.han.ica.icss.ast.ASTNode;
+import nl.han.ica.icss.ast.Declaration;
 import nl.han.ica.icss.ast.types.ExpressionType;
 
 import java.util.HashMap;
@@ -22,11 +23,29 @@ public class Checker {
 
 
     public void checkSemantics(ASTNode node){
+        for(ASTNode child: node.getChildren()){
+            checkDeclaration(child);
+            if(!node.getChildren().isEmpty()){
+                checkSemantics(child);
+            }
+        }
+
+
+
 
     }
 
 
-    public void checkVariableDeclaration() {
+    public void checkDeclaration(ASTNode declaration) {
+        if(declaration instanceof Declaration){
+            String propertyName = declaration.getChildren().get(0).getNodeLabel();
+            System.out.println(propertyName);
+
+
+        }
+
+
+
 
     }
 
