cd65182fe80fe2cb5cb18ce0426b5f7e8722c057
Marcus Bosveld <mjh.bosveld@student.han.nl>
2024-03-29 12:09:29 +0000

    Generator af en helemaal gefixt

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,15 +1,59 @@
 package nl.han.ica.icss.generator;
 
 
-import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.ColorLiteral;
+import nl.han.ica.icss.ast.literals.PercentageLiteral;
+import nl.han.ica.icss.ast.literals.PixelLiteral;
 
 public class Generator {
+	private boolean inStylerule = false;
 
 	public String generate(AST ast) {
-        return "";
-
+		StringBuilder sb = new StringBuilder();
+        return generateString(ast.root, sb).toString();
 
 	}
 
-	
+	public StringBuilder generateString(ASTNode node, StringBuilder sb){
+
+		makeStyleRule(node, sb);
+		makeDeclaration(node, sb);
+
+		for(ASTNode child: node.getChildren()){
+			if(!node.getChildren().isEmpty()){
+				generateString(child, sb);
+			}
+
+		}
+		if (node instanceof Stylerule){
+			inStylerule = false;
+		}
+		endStyleRule(node, sb);
+		return sb;
+	}
+
+
+	public void makeStyleRule(ASTNode node, StringBuilder sb) {
+		if (node instanceof Stylerule) {
+			sb.append(((Stylerule) node).selectors.get(0).toString() + " {\n");
+			inStylerule = true;
+		}
+	}
+	public void makeDeclaration(ASTNode node, StringBuilder sb){
+		if(node instanceof Declaration){
+			if(((Declaration)node).expression instanceof ColorLiteral){
+				sb.append("    " + ((Declaration)node).property.name + ": " + ((ColorLiteral)((Declaration)node).expression).value + ";\n");
+			}else if (((Declaration)node).expression instanceof PixelLiteral){
+				sb.append("    " + ((Declaration)node).property.name + ": " + ((PixelLiteral)((Declaration)node).expression).value + "px;\n");
+			}else if (((Declaration)node).expression instanceof PercentageLiteral){
+				sb.append("    " + ((Declaration)node).property.name + ": " + ((PercentageLiteral)((Declaration)node).expression).value + "%;\n");
+			}
+		}
+	}
+	public void endStyleRule(ASTNode node, StringBuilder sb){
+		if(!inStylerule && node instanceof Stylerule){
+			sb.append("}\n");
+		}
+	}
 }
